db625f7e706198db61f88d1e5943e6ab74150545
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-07-17 10:25:29 +1200

    worked even more on country dialogues

--- AI/Country_Manager.gd
+++ AI/Country_Manager.gd
@@ -1,3 +1,23 @@
 extends Node
 
-var queued_dialogue = []
+var queued_dialogue : Array = []
+var event_timer : Timer = Timer.new()
+@onready var dialogue_manager = get_parent().get_parent().get_parent().get_node("DialogueContainer")
+
+var min_delay_time = 1.4
+var max_delay_time = 12
+
+func _ready() -> void:
+	event_timer.autostart = false
+	event_timer.one_shot = true
+	add_child(event_timer)
+	event_timer.connect("timeout",choose_new)
+	event_timer.start(randf_range(min_delay_time,max_delay_time))
+	pass
+
+func choose_new() -> void:
+	if GlobalDialogue.talking != true and len(queued_dialogue) > 0:
+		var rand_ind = randi_range(0,len(queued_dialogue)-1)
+		var new_dialogue = queued_dialogue.pop_at(rand_ind)
+		dialogue_manager.load_dialogues(new_dialogue[0],new_dialogue[1])
+	event_timer.start(randf_range(min_delay_time,max_delay_time))
--- AI/mendzova.gd
+++ AI/mendzova.gd
@@ -1,1 +1,10 @@
 extends Node
+
+@onready var manager = get_parent()
+var last
+
+func _ready() -> void:
+	manager.queued_dialogue.append(["Mendzova","Introduction"])
+
+func process() -> void:
+	pass
